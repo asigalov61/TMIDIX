3bc2b85c1b23cb4206cb175bdd02610271393774
Alex <alexlev61@proton.me>
2026-03-15 02:06:53 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-# docs/conf.py
-
 import os
 import sys
+
+# Add project root so Sphinx can import TMIDIX
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "TMIDIX"
@@ -11,12 +11,17 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
 ]
 
 autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
